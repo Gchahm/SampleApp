93e4dacc1d69b7f9b301024fe10dee64f5ec550d
= <=>
2018-09-19 18:55:08 +0000

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -12,13 +12,19 @@ Rails.application.routes.draw do
   # Users routes
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
-  resources :users
+  #resources :users
   # Sessions routes
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   #account Activation
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 end
